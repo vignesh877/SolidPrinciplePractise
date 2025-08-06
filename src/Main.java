4982c86fe40ffed3c54c3a21339842e9e5c638ff
Vignesh <69396223+vignesh877@users.noreply.github.com>
2025-08-06 12:17:01 +0530

    Delete src directory

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        NotificationSevice notificationSevice = new NotificationSevice(new WhatsappNotification());
-        notificationSevice.sendNotification();
-    }
-}
